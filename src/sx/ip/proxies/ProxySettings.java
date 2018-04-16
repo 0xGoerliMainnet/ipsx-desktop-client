e0cf5947b3c588d7eb447a6f0e20561dafda845b
Hygor Carvalho da Luz <hygor.c.luz@gmail.com>
2018-04-16 14:39:59 -0300

    Improve the Javadoc in ProxySettings class

--- src/sx/ip/proxies/ProxySettings.java
+++ src/sx/ip/proxies/ProxySettings.java
@@ -1,18 +1,50 @@
 package sx.ip.proxies;
 
 /**
- *
+ * Class that will storage all Proxy settings for the system.
  */
 public class ProxySettings {
 
+    /** The proxy host for the connection. */
     private final String proxyHost;
+    
+    /** The proxy port to be access. */
     private final Integer proxyPort;
+    
+    /** The proxy type witch can be HTTP, HTTPS ,Socks, FTP, HTTP and HTTPS. */
     private final ProxyType type;
+    
+    /** A url for get all proxy information at once. */
     private final String acsUrl;
+    
+    /** A bypass on local environment. */
     private final boolean bypassOnLocal;
+    
+    /** The root user for possible authorization requirements. */
     private final String authUser;
+    
+    /** The root password for possible authorization requirements. */
     private final String authPass;
-
+    
+    
+    /**
+    * Create a <code>ProxySettings</code> that will hold all Proxy configuration for the system.
+    *
+    * @param proxyHost
+    *            The proxy host for the connection
+    * @param proxyPort
+    *            The proxy port to be access
+    * @param type
+    *            The proxy type witch can be HTTP, HTTPS ,Socks, FTP, HTTP and HTTPS.
+    * @param acsUrl
+    *            A url for get all proxy information at once
+    * @param bypassOnLocal
+    *            A bypass on local environment
+    * @param authUser
+    *            The root user for possible authorization requirements
+    * @param authPass
+    *            The root password for possible authorization requirements
+    */
     public ProxySettings(String proxyHost, Integer proxyPort, ProxyType type, String acsUrl, boolean bypassOnLocal, String authUser, String authPass) {
         this.proxyHost = proxyHost;
         this.proxyPort = proxyPort;
@@ -32,7 +64,11 @@ public class ProxySettings {
     public static ProxySettings getDirectConnectionSetting() {
         return new ProxySettings(null, null, null, null, true, null, null);
     }
-
+    
+    /**
+     * All Proxy types that can be used.
+     *
+     */
     public enum ProxyType {
         SOCKS, HTTP, HTTPS, FTP, HTTP_AND_HTTPS;
     }
