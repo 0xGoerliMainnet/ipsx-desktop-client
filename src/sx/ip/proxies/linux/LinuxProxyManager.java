ba147885ba8d7ea0612fa39f79ab85d358ac853e
Hygor Carvalho da Luz <hygor.c.luz@gmail.com>
2018-04-18 17:09:07 -0300

    Linux proxy setting commands

--- src/sx/ip/proxies/linux/LinuxProxyManager.java
+++ src/sx/ip/proxies/linux/LinuxProxyManager.java
@@ -36,7 +36,7 @@ public class LinuxProxyManager extends ProxyManager {
      *
      * @param settings A ProxySettings instance that hold all necessary
      * configurations
-     * 
+     *
      * @throws ProxySetupException The Proxy setup exception
      */
     @Override
@@ -58,109 +58,134 @@ public class LinuxProxyManager extends ProxyManager {
      * Main function.
      *
      * @param args the command line arguments
-     * 
+     *
      * @throws IOException The IO exception
-     * 
+     *
      * @throws InterruptedException The interrupted exception
      */
-    public static void main(String[] args) throws IOException, InterruptedException {
-
-        setInternetProxy("127.0.0.1", "8080");
-        disableInternetProxy();
+    public static void main(String[] args) throws IOException, InterruptedException, ProxySetupException {
+        ProxySettings settings = new ProxySettings("127.0.0.1", 8080, ProxySettings.ProxyType.HTTP, null, true, null, null);
+        setInternetProxy(settings);
+        //disableInternetProxy();
 
     }
-    
+
     /**
      * Method resposible for start the Proxy connection.
      *
-     * @param ip the ip for do the proxy connection
-     * 
-     * @param port the port for the proxy connection
-     * 
+     * @param settings A ProxySettings instance that hold all necessary
+     * configurations
+     *
      * @throws IOException The IO exception
-     * 
+     *
      * @throws InterruptedException The interrupted exception
      */
-    public static void setInternetProxy(String ip, String port) throws IOException, InterruptedException {
+    public static void setInternetProxy(ProxySettings settings) throws IOException, InterruptedException {
         //Just a test call for valid the command line execution
-        
+
         StringBuilder stringCommand = new StringBuilder();
-        stringCommand.append("export http_proxy=\"http://");
-        stringCommand.append(ip);
-        stringCommand.append(":");
-        stringCommand.append(port);
-        stringCommand.append("\"");
-        stringCommand.append(" & ");
+        if (settings.getProxyHost() != null) {
+            stringCommand.append("gsettings set org.gnome.system.proxy mode 'manual'");
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy.http enabled true");
+            stringCommand.append(" & ");
+
+            switch (settings.getType()) {
+                case HTTP:
+                    stringCommand.append("gsettings set org.gnome.system.proxy.http host 'proxyHost'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.http port 'proxyPort'");
+                    break;
+                case HTTPS:
+                    stringCommand.append("gsettings set org.gnome.system.proxy.https host 'proxyHost'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.https port 'proxyPort'");
+                    break;
+                case SOCKS:
+                    stringCommand.append("gsettings set org.gnome.system.proxy.socks host 'proxyHost'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.socks port 'proxyPort'");
+                    break;
+                case FTP:
+                    stringCommand.append("gsettings set org.gnome.system.proxy.ftp host 'proxyHost'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.ftp port 'proxyPort'");
+                    break;
+                case HTTP_AND_HTTPS:
+                    stringCommand.append("gsettings set org.gnome.system.proxy.http host 'proxyHost'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.http port 'proxyPort'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.https host 'proxyHost'");
+                    stringCommand.append(" & ");
+                    stringCommand.append("gsettings set org.gnome.system.proxy.https port 'proxyPort'");
+                    break;
+            }
+        } else {
+            stringCommand.append("gsettings set org.gnome.system.proxy mode 'none'");
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy.http enabled false");
+        }
+
+        if ((settings.getAuthUser() != null) && (settings.getAuthUser().isEmpty())) {
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy.http authentication-user 'userName'");
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy.http authentication-password 'userPassword'");
+        }
+
+        if (settings.getBypassOnLocal()) {
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy ignore-hosts \"['localhost',  '127.0.0.1', 'all', 'other', 'hosts']\"");
+        }
         
-        stringCommand.append("export https_proxy=\"http://");
-        stringCommand.append(ip);
-        stringCommand.append(":");
-        stringCommand.append(port);
-        stringCommand.append("\"");
-        stringCommand.append(" & ");
+        if((settings.getAcsUrl() != null) && (settings.getAcsUrl().isEmpty())){
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy mode 'auto'");
+            stringCommand.append(" & ");
+            stringCommand.append("gsettings set org.gnome.system.proxy autoconfig-url 'acsUrl'");
+        }
+
+        String commandLine = stringCommand.toString()
+                .replaceAll("proxyHost", settings.getProxyHost())
+                .replaceAll("proxyPort", String.valueOf(settings.getProxyPort())
+                .replaceAll("userName", settings.getAuthUser())
+                .replaceAll("userPassword", settings.getAuthPass())
+                .replaceAll("acsUrl", settings.getAcsUrl()));
         
-        stringCommand.append("export ftp_proxy=\"http://");
-        stringCommand.append(ip);
-        stringCommand.append(":");
-        stringCommand.append(port);
-        stringCommand.append("\"");
-        stringCommand.append(" & ");
-        
-        stringCommand.append("env | grep -i proxy");
-        stringCommand.append(" & ");
-        stringCommand.append("wget -q -O - checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'");        
-        stringCommand.append(" & ");
-        stringCommand.append("time wget -q -O - checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'");
-        
-        runCommandLine(stringCommand.toString(), null, null, null, 3000);
-    }    
-    
+        System.err.println(commandLine);
+        runCommandLine(commandLine, null, null, null, 3000);
+    }
+
     /**
      * Method resposible for disable the Proxy connection.
-     * 
+     *
      * @throws IOException The IO exception
-     * 
+     *
      */
     public static void disableInternetProxy() throws IOException {
         StringBuilder stringCommand = new StringBuilder();
-        stringCommand.append("unset http_proxy");
+        stringCommand.append("gsettings set org.gnome.system.proxy mode 'none'");
         stringCommand.append(" & ");
-        
-        stringCommand.append("unset https_proxy");
-        stringCommand.append(" & ");
-        
-        stringCommand.append("unset ftp_proxy");
-        stringCommand.append(" & ");
-        
-        stringCommand.append("env | grep -i proxy");
-        stringCommand.append(" & ");
-        stringCommand.append("wget -q -O - checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'");        
-        stringCommand.append(" & ");
-        stringCommand.append("time wget -q -O - checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//'");
-        
-        runCommandLine(stringCommand.toString(), null, null, null, 3000);
-    }    
-    
+        stringCommand.append("gsettings set org.gnome.system.proxy.http enabled false");
+        System.err.println(stringCommand.toString());
+        //runCommandLine(stringCommand.toString(), null, null, null, 3000);
+    }
+
     /**
      * Method responsible for execute the command lines.
      *
-     * @param cmd 
-     *           The command line to be executed 
-     * @param args 
-     *           Arguments for the command line 
-     * @param pumpStreamHandle 
-     *           Handle with sub-processes output and errors
-     * @param executeResultHandle 
-     *           Handle with the execution result
-     * @param timeout 
-     *           The watchdog timeout
-     * @throws IOException 
-     *           The IO exception
-     * 
+     * @param cmd The command line to be executed
+     * @param args Arguments for the command line
+     * @param pumpStreamHandle Handle with sub-processes output and errors
+     * @param executeResultHandle Handle with the execution result
+     * @param timeout The watchdog timeout
+     * @throws IOException The IO exception
+     *
      */
     private static void runCommandLine(String cmd, String[] args, PumpStreamHandler pumpStreamHandle,
             ExecuteResultHandler executeResultHandle, int timeout) throws IOException {
-        System.out.println("Test sequence starting....");
+        System.out.println("Test commands starting....");
 
         executor = new DefaultExecutor();
 
