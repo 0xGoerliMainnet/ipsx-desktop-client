01bdf82b4e2bdcdb7180c219e2e267dce9636d91
Hygor Carvalho da Luz <hygor.c.luz@gmail.com>
2018-04-26 15:51:37 -0300

    setProxySettings implementation
    - Implementation of method setProxySettings
    - Implementation of method runPowerShellMethod
    - Javadoc

--- src/sx/ip/proxies/windows/WindowsProxyManager.java
+++ src/sx/ip/proxies/windows/WindowsProxyManager.java
@@ -18,6 +18,10 @@ import com.profesorfalken.jpowershell.PowerShellNotAvailableException;
 import com.profesorfalken.jpowershell.PowerShellResponse;
 import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.io.IOUtils;
 import sx.ip.proxies.ProxyManager;
 import sx.ip.proxies.ProxySettings;
@@ -32,7 +36,13 @@ public class WindowsProxyManager extends ProxyManager {
      */
     @Override
     public boolean setProxySettings(ProxySettings settings) throws ProxySetupException {
-        throw new UnsupportedOperationException("Not supported yet.");
+       boolean commandResult = false;
+        try {
+            commandResult = setInternetProxy(settings);
+        } catch (IOException ex) {
+            Logger.getLogger(WindowsProxyManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return commandResult;
     }
 
     /**
@@ -45,58 +55,110 @@ public class WindowsProxyManager extends ProxyManager {
 
     /**
      * @param args the command line arguments
+     * @throws IOException the IO exception
      */
     public static void main(String[] args) throws IOException {
-
-        setInternetProxy("127.0.0.1", "8080", "");
+        ProxySettings settings = new ProxySettings("127.0.0.1", 8080, ProxySettings.ProxyType.HTTP, null, true, null, null);
+        setInternetProxy(settings);
         disableInternetProxy();
 
     }
 
-    public static void setInternetProxy(String ip, String port, String acs) throws IOException {
-        String cmd = IOUtils.resourceToString("/sx/ip/ipsxclient/scripts/windows-set-internet-proxy.ps1", Charset.forName("UTF-8"));
-
-        PowerShell powerShell = null;
-        try {
-            //Creates PowerShell session (we can execute several commands in the same session)
-            powerShell = PowerShell.openSession();
-
-            //Execute a command in PowerShell session
-            powerShell.executeCommand(cmd);
-
-            //Execute another command in the same PowerShell session
-            PowerShellResponse response = powerShell.executeCommand("Set-InternetProxy -proxy \"socks=" + ip + ":" + port + "\" " + (acs == null || acs.isEmpty() ? "-acs \"" + acs + "\"" : ""));
-
-            //Print results
-            System.out.println("Output 2:" + response.getCommandOutput());
-        } catch (PowerShellNotAvailableException ex) {
-            //Handle error when PowerShell is not available in the system
-            //Maybe try in another way?
-            ex.printStackTrace();
-        } finally {
-            //Always close PowerShell session to free resources.
-            if (powerShell != null) {
-                powerShell.close();
-            }
+    /**
+     * Method resposible for start the Proxy connection.
+     *
+     * @param settings A ProxySettings instance that hold all necessary
+     * configurations
+     *
+     * @return If the command was executed or not
+     *
+     * @throws IOException The IO exception
+     */
+    public static boolean setInternetProxy(ProxySettings settings) throws IOException {
+        String result = "false";
+        switch (settings.getType()) {
+            case HTTP:
+                result = runPowerShellMethod("http", settings, false).get("result");
+                break;                
+            case HTTPS:
+                result = runPowerShellMethod("https", settings, false).get("result");
+                break;
+            case SOCKS:
+                result = runPowerShellMethod("socks", settings, false).get("result");
+                break;
+            case FTP:
+                result = runPowerShellMethod("ftp", settings, false).get("result");
+                break;
+            case HTTP_AND_HTTPS:
+                result = runPowerShellMethod("http", settings, false).get("result");
+                if("true".equals(result)){
+                    result = runPowerShellMethod("https", settings, false).get("result");
+                }
+                break;
         }
+        return "true".equals(result);
     }
-
-    public static void disableInternetProxy() throws IOException {
-        String cmd = IOUtils.resourceToString("/sx/ip/ipsxclient/scripts/windows-disable-internet-proxy.ps1", Charset.forName("UTF-8"));
-
+    
+    /**
+     * Method resposible for disable the Proxy connection.
+     *
+     * @return If the command was executed or not
+     *
+     * @throws IOException The IO exception
+     *
+     */
+    public static boolean disableInternetProxy() throws IOException {        
+        
+        return "true".equals(runPowerShellMethod(null, null, true).get("result"));
+    }
+    
+    /**
+     * Function responsible for execute the calls to the PowerShell Script Methods.
+     *
+     * @param proxyType Type of proxy, what can be (http, https, socks and ftp)
+     * 
+     * @param settings ProxySettings instance containing all proxy configurations
+     * 
+     * @param disable Flag to indicate which script to use, what can be the set or disable scripts
+     *
+     * @return A map result and output message
+     */
+    private static Map<String, String> runPowerShellMethod( String proxyType, ProxySettings settings, boolean disable) throws IOException {
+        System.out.println("Test commands starting....");
+        Map<String, String> response = new HashMap<>();
+        String powerShellScript;
+        String psMethod;
         PowerShell powerShell = null;
+        
+        if(disable){
+            psMethod = "Disable-InternetProxy";
+            powerShellScript = IOUtils.resourceToString("/sx/ip/ipsxclient/scripts/windows-disable-internet-proxy.ps1", Charset.forName("UTF-8"));
+        }else{
+            psMethod = "Set-InternetProxy -proxy \"" +proxyType +"=" + settings.getProxyHost() + ":" + settings.getProxyPort() + "\" " 
+                       + (settings.getAcsUrl() != null && !settings.getAcsUrl().isEmpty() ? "-acs \"" + settings.getAcsUrl() + "\"" : "");
+            
+            powerShellScript = IOUtils.resourceToString("/sx/ip/proxies/windows/scripts/windows-set-internet-proxy.ps1", Charset.forName("UTF-8"));
+        }        
+        
         try {
             //Creates PowerShell session (we can execute several commands in the same session)
             powerShell = PowerShell.openSession();
 
             //Execute a command in PowerShell session
-            powerShell.executeCommand(cmd);
-
+            powerShell.executeCommand(powerShellScript);
+            
             //Execute another command in the same PowerShell session
-            PowerShellResponse response = powerShell.executeCommand("Disable-InternetProxy");
+            PowerShellResponse psResponse = powerShell.executeCommand(psMethod);
+            
+            if(!psResponse.isError()){
+                 response.put("result", "false");
+            } else {
+                response.put("result", "true");
+            }
 
             //Print results
-            System.out.println("Output 2:" + response.getCommandOutput());
+            System.out.println("Output:" + psResponse.getCommandOutput());
+            response.put("output", psResponse.getCommandOutput());
         } catch (PowerShellNotAvailableException ex) {
             //Handle error when PowerShell is not available in the system
             //Maybe try in another way?
@@ -107,6 +169,8 @@ public class WindowsProxyManager extends ProxyManager {
                 powerShell.close();
             }
         }
+
+        return response;
     }
 
 }
