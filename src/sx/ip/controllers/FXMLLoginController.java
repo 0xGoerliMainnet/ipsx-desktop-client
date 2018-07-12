75205c0a6cad1f896247f9cdfca7ac361bb5d377
Caio Fontes <olixis@gmail.com>
2018-07-12 16:35:30 -0300

    IDC-96 Finishing login screen layout
    Finishing login screen layout and actions

--- src/sx/ip/controllers/FXMLLoginController.java
+++ src/sx/ip/controllers/FXMLLoginController.java
@@ -14,11 +14,17 @@
 package sx.ip.controllers;
 
 import com.jfoenix.controls.JFXButton;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.layout.AnchorPane;
+import sx.ip.IPSXDesktopClient;
+import sx.ip.factories.HostServicesControllerFactory;
+import sx.ip.utils.ProxyUtils;
 
 /**
  *
@@ -38,11 +44,43 @@ public class FXMLLoginController extends NavController implements Initializable{
     /** The login info pane instance.  */
     @FXML
     private AnchorPane loginInfoPane;
+    
+    /** The close button instance.  */
+    @FXML
+    private JFXButton btnClose;
 
     /** The main anchor pane instance.  */
     @FXML
     private AnchorPane mainAnchorPane;
     
+    /**
+    * Method resposible for handling the close action.
+    *
+    * @param event 
+    *          An Event representing that the button has been fired.
+    */
+    @FXML
+    private void handleCloseAction(ActionEvent event) {
+        stage.close();
+    }
+    
+    /**
+    * Method resposible for the transition to the login with email screen action.
+    *
+    * @param event
+    *          An Event representing that the button has been fired.
+    */
+    @FXML
+    private void loginWithEmailAction(ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(IPSXDesktopClient.class.getResource("resources/fxml/FXMLManualProxy.fxml"), ProxyUtils.getBundle());
+        loader.setControllerFactory(new HostServicesControllerFactory(app.getHostServices()));
+        NavControllerHandle.navigateTo(loader, stage, app);
+    }
+    
+    @FXML
+    private void loginWithFacebookAction(ActionEvent ae) throws IOException{
+        
+    }
     
 
 
